ce9ef1fdbc15a641c2dfce8a6d2be08517570998
Sabadus <sabadus_calin@yahoo.com>
2019-12-22 23:11:37 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        {
+            // write your code here
+            //1. Printati "Hello" in consola, iar pe rand nou, printati numele vostru.
+            System.out.println("Hello");
+            System.out.println("Calin.Sabadus");
+            // 2. Printati rezultatul sumei a doua numere(orice numere)
+            int a = 2;
+            int b = 4;
+            int sum = a + b;
+            System.out.println(sum);
+            //3. Printati rezultatul impartirii a doua numere
+            int x = 21;
+            int y = 3;
+            int tot = x / y;
+            System.out.println(tot);
+            // Printati rezultatul urmatoarelor operatiuni:
+            //
+            //a. -5 + 8 * 6
+            int d = -5;
+            int e = 8;
+            int f = 6;
+            int rez = d + e * f;
+            System.out.println(rez);
+            // b. (55+9) % 9
+            int w = 55;
+            int q = 9;
+            int cat = w + q;
+            System.out.println(cat % +q);
+            //c. 20 + -3*5 / 8
+            int t = 20;
+            int u = -3;
+            int i = 5;
+            int o = 8;
+            int dog = t + u * i / o;
+            System.out.println(dog);
+            //d. 5 + 15 / 3 * 2 - 8 % 3
+            int m = 15;
+            int n = 3;
+            int v = 2;
+            int suma = i + m / n * v - o % +n;
+            System.out.println(suma);
+        }
+
+
+    }
+
+}
